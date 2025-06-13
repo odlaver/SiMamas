4486edf841170a851378197ea665cc8d76523652
revaldo <aldo110606@gmail.com>
2025-06-13 14:43:07 +0700

    finalisasi main

--- Finale/main.cpp
+++ Finale/main.cpp
@@ -21,30 +21,61 @@ int main() {
         cout << "Pilih opsi (1-9): ";
         cin >> pilihan;
 
+        while (!(cin >> pilihan) || pilihan < 1 || pilihan > 9) {
+            cout << "\nOpsi tidak valid.\n";
+            cout << "Silakan pilih opsi yang tersedia (1-9): ";
+            cin.clear(); 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
         switch (pilihan) {
             case 1:
                 sistem.daftar();
+                cout << "\nTunggu sebentar..";
+                sleep(2);
+                sistem.clearScreen();
                 break;
             case 2:
                 sistem.lihatAntrian();
+                cout << "\nTunggu sebentar..";
+                sleep(4);
+                sistem.clearScreen();
                 break;
             case 3:
                 sistem.prosesPasien();
+                cout << "\nPasien sedang diproses...";
+                sleep(1);
+                cout << "\nPasien telah diproses. Terima kasih telah menunggu.\n";
+                sleep(2);
+                sistem.clearScreen();
                 break;
             case 4:
                 sistem.riwayatPelayanan();
+                cout << "\nTunggu sebentar..";
+                sleep(4);
+                sistem.clearScreen();
                 break;
             case 5:
                 sistem.cariPasien();
+                sleep(2);
+                sistem.clearScreen();
                 break;
             case 6:
                 sistem.editPasien();
+                cout << "\nTunggu sebentar..";
+                sleep(2);
+                sistem.clearScreen();
                 break;
             case 7:
                 sistem.hapusPasien();
+                cout << "\nTunggu sebentar..";
+                sleep(2);
+                sistem.clearScreen();
                 break;
             case 8:
                 sistem.statistikPuskes();
+                sleep(4);
+                sistem.clearScreen();
                 break;
             case 9:
                 cout << "\nTerima kasih telah berkonsultasi!\n";
